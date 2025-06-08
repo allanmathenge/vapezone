0d1cac3dc7181b05f6333f1203ff46951aefab54
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-06-08 11:11:18 +0300

    decorated hero page

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,25 +1,30 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+import { Heading } from "@medusajs/ui"
+import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[75vh] aspect-[29/34] text-white w-full overflow-hidden border-b border-ui-border-base relative bg-ui-bg-subtle">
+      {/* bg */}
+      <Image
+        src="/images/vape_on_ground.jpg"
+        priority={true}
+        className="absolute inset-0"
+        quality={100}
+        // width={1920}
+        alt={"Hero image"}
+        fill
+        sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+        style={{
+          objectFit: "cover",
+        }}
+      />
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Wellcome to vapezone
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            your one stop shop for all your vaping needs
-          </Heading>
-        </span>
-       
+        <Heading className="text-ui-fg-base text-compact-xlarge-plus font-bold">
+          VAPEZONE
+        </Heading>
+        <p className="text-ui-fg-subtle text-white text-small-regular max-w-[600px]">
+          Your one-stop shop for all your vaping needs.
+        </p>
       </div>
     </div>
   )
