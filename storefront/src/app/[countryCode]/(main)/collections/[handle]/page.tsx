a2a8c9cab63bbe510e62633a70b2d6983d438e1a
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-06-08 04:11:20 +0300

    Init commit

--- storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
@@ -59,7 +59,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 
   const metadata = {
-    title: `${collection.title} | Medusa Store`,
+    title: `${collection.title} | Vapezone`,
     description: `${collection.title} collection`,
   } as Metadata
 
