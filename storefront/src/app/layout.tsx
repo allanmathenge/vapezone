110fbbf0ab0110942f484274bc72cb0a25e037e9
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-06-17 22:41:51 +0300

    Changed opengraph-images descriptions

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -4,7 +4,6 @@ import "styles/globals.css"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
-  description: "Discover VapeZone – your go-to destination for premium vaping products. Explore a wide range of cutting-edge vape devices, rich-flavored e-liquids, and accessories tailored for every vaping enthusiast. Enjoy expert insights, exclusive deals, and top-tier brands. Elevate your vaping experience today!",
   twitter: {
     card: "summary_large_image"
   }
