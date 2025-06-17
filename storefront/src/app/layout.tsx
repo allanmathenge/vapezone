1b92e92902a6690c7453866385e6f4c946e98a0f
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-06-17 22:13:25 +0300

    Added app previews

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -4,6 +4,10 @@ import "styles/globals.css"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
+  description: "Discover VapeZone – your go-to destination for premium vaping products. Explore a wide range of cutting-edge vape devices, rich-flavored e-liquids, and accessories tailored for every vaping enthusiast. Enjoy expert insights, exclusive deals, and top-tier brands. Elevate your vaping experience today!",
+  twitter: {
+    card: "summary_large_image"
+  }
 }
 
 export default function RootLayout(props: { children: React.ReactNode }) {
