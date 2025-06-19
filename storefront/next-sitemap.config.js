aa9d46fe7a2bf662f6b79397a2d23339ddac30aa
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-06-19 11:24:56 +0300

    Deleted sitemaps

--- storefront/next-sitemap.config.js
+++ storefront/next-sitemap.config.js
@@ -1,10 +1,9 @@
 const excludedPaths = ["/checkout", "/account/*", "/checkout", "/opengraph-image.png", "/api/healthcheck"]
 
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://www.vapezone.co.ke/ke",
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://www.vapezone.co.ke",
   generateRobotsTxt: true,
   generateIndexSitemap: true,
-  sitemapSize: 7000,
   exclude: [ ...excludedPaths, "/[sitemap]"],
   robotsTxtOptions: {
     policies: [
