baccbae8aa6db0b4cce11c8c9931a3017aef7132
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-06-20 12:01:16 +0300

    Sitemaps and XML files fix

--- storefront/next-sitemap.config.js
+++ storefront/next-sitemap.config.js
@@ -1,4 +1,4 @@
-const excludedPaths = ["/checkout", "/account/*", "/checkout", "/opengraph-image.png", "/api/healthcheck"]
+const excludedPaths = ["/checkout", "/account/*", "/opengraph-image.png", "/api/healthcheck"]
 
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://www.vapezone.co.ke",
--- storefront/src/app/sitemap.ts
+++ storefront/src/app/sitemap.ts
@@ -1,12 +1,29 @@
+
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    return [
-        {
-            url: ""
-        }
-    ].map((route: MetadataRoute.Sitemap[number]) => ({
-        ...route,
-        url: "https://www.vapezone.co.ke" + route.url
-    }))
+  const baseUrl = "https://www.vapezone.co.ke";
+
+  // List your site paths here
+  const paths = ["", "/store",];
+
+  return paths.map((path) => ({
+    url: `${baseUrl}${path}`,
+    lastModified: new Date().toISOString(),
+  }));
 }
+
+
+// import type { MetadataRoute } from "next";
+
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//     return [
+//         {
+//             url: ""
+//         }
+//     ].map((route: MetadataRoute.Sitemap[number]) => ({
+//         ...route,
+//         url: "https://www.vapezone.co.ke" + route.url,
+//         lastModified: new Date().toISOString(),
+//     }))
+// }
