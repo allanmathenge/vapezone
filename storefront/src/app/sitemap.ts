fe72b4fa0961dbfc9643ba93cf1d12783eac7b2b
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-06-21 08:45:41 +0300

    Updates to package.json

--- storefront/src/app/sitemap.ts
+++ storefront/src/app/sitemap.ts
@@ -12,18 +12,3 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     lastModified: new Date().toISOString(),
   }));
 }
-
-
-// import type { MetadataRoute } from "next";
-
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//     return [
-//         {
-//             url: ""
-//         }
-//     ].map((route: MetadataRoute.Sitemap[number]) => ({
-//         ...route,
-//         url: "https://www.vapezone.co.ke" + route.url,
-//         lastModified: new Date().toISOString(),
-//     }))
-// }
