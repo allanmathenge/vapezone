0c286a8d0fb828389fb2ba5e139ccb86f583d5c9
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-07-15 07:21:07 +0300

    Fixing product info

--- storefront/src/modules/products/components/product-tabs/index.tsx
+++ storefront/src/modules/products/components/product-tabs/index.tsx
@@ -47,22 +47,22 @@ const ProductInfoTab = ({ product }: ProductTabsProps) => {
       <div className="grid grid-cols-2 gap-x-8">
         <div className="flex flex-col gap-y-4">
           <div>
-            <span className="font-semibold">Puffs</span>
+            <span className="font-semibold">Puff Count</span>
             <p>{product.material ? product.material : "-"}</p>
           </div>
           <div>
-            <span className="font-semibold">Country of origin</span>
+            <span className="font-semibold">Country Of Origin</span>
             <p>{product.origin_country ? product.origin_country : "-"}</p>
           </div>
           <div>
-            <span className="font-semibold">Type</span>
+            <span className="font-semibold">Nicotine Strength</span>
             <p>{product.type ? product.type.value : "-"}</p>
           </div>
         </div>
         <div className="flex flex-col gap-y-4">
           <div>
-            <span className="font-semibold">Weight</span>
-            <p>{product.weight ? `${product.weight} g` : "-"}</p>
+            <span className="font-semibold">Battery Capacity</span>
+            <p>{product.weight ? `${product.weight} mAh` : "-"}</p>
           </div>
           <div>
             <span className="font-semibold">Dimensions</span>
