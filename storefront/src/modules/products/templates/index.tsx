a5b0ac89d2450e0b94ec165dce79b9e753715b2c
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-07-17 03:17:54 +0300

    Reversed product thumbnail and desc

--- storefront/src/modules/products/templates/index.tsx
+++ storefront/src/modules/products/templates/index.tsx
@@ -32,13 +32,16 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
         className="content-container flex flex-col small:flex-row small:items-start py-6 relative"
         data-testid="product-container"
       >
-        <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
-          <ProductInfo product={product} />
-          <ProductTabs product={product} />
+        <div className="content-container flex flex-col-reverse small:flex-row">
+          <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
+            <ProductInfo product={product} />
+            <ProductTabs product={product} />
+          </div>
+          <div className="block w-full relative">
+            <ImageGallery images={product?.images || []} />
+          </div>
         </div>
-        <div className="block w-full relative">
-          <ImageGallery images={product?.images || []} />
-        </div>
+
         <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-12">
           <ProductOnboardingCta />
           <Suspense
