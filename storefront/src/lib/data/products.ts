1d3a815f6168805bfb3a7ad8b20dfaf5d7d73fda
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-07-26 05:28:42 +0300

    Okay

--- storefront/src/lib/data/products.ts
+++ storefront/src/lib/data/products.ts
@@ -66,7 +66,7 @@ export const getProductsList = cache(async function ({
   nextPage: number | null
   queryParams?: HttpTypes.FindParams & HttpTypes.StoreProductParams
 }> {
-  const limit = queryParams?.limit || 240
+  const limit = queryParams?.limit || 360
   const validPageParam = Math.max(pageParam, 1);
   const offset = (validPageParam - 1) * limit
   const region = await getRegion(countryCode)
