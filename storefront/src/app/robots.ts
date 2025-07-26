18ddf3fb42741aff0b6d5e59bd2fcf4a4740aa74
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-07-26 07:03:50 +0300

    sitemaps

--- storefront/src/app/robots.ts
+++ storefront/src/app/robots.ts
@@ -18,6 +18,6 @@ export default function robots(): MetadataRoute.Robots {
         ],
       },
     ],
-    sitemap: 'https://www.vapezone.co.ke/sitemap.xml',
+    sitemap: 'https://www.vapezone.co.ke/sitemap.xml/',
   }
 }
