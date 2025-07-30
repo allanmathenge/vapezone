213258437d3fe2b90e1985861f346fa4a18441ac
ALLAN MATHENGE <allanmathenge2@gmail.com>
2025-07-30 10:16:54 +0300

    Improved titles UI

--- storefront/src/modules/products/components/product-preview/index.tsx
+++ storefront/src/modules/products/components/product-preview/index.tsx
@@ -39,7 +39,7 @@ export default async function ProductPreview({
           isFeatured={isFeatured}
         />
         <div className="flex flex-col txt-compact-medium mt-4 justify-between">
-          <Text className="text-ui-fg-subtle" data-testid="product-title">
+          <Text className="text-ui-fg-subtle text-nowrap overflow-hidden text-ellipsis" data-testid="product-title">
             {product.title}
           </Text>
           <div className="flex items-center gap-x-2">
